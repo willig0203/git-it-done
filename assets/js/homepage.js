ef52e481724423af8a3e8339ec5b921e639adb1f
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-06-01 12:57:34 -0500

    format uri

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,8 +1,13 @@
-var getUserRepos = function () {
-    var getUserRepos = function () {
-        fetch("https://api.github.com/users/octocat/repos");
-    };
-    console.log("function was called");
+var getUserRepos = function (user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    });
 };
 
 getUserRepos();
