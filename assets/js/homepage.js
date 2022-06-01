c1269e4bf5a1a90c8dbc11a72a4e3732f1906669
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-06-01 11:53:33 -0500

    added fetch to js

---
+++ assets/js/homepage.js
@@ -0,0 +1,8 @@
+var getUserRepos = function () {
+    var getUserRepos = function () {
+        fetch("https://api.github.com/users/octocat/repos");
+    };
+    console.log("function was called");
+};
+
+getUserRepos();
